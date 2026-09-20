19dffc15db5a63a3d19997ae5696c2efeba43801
alexis-gonzalez-code <otibenites939@gmail.com>
2026-09-20 15:15:43 -0600

    update inversiones_G&G.py

--- inversiones_G&G.py
+++ inversiones_G&G.py
@@ -5,7 +5,7 @@
 # añadir recordatorios en partes especificas del programa
 # AGG CON LA LIBRERIA RANDOM DIAS RANDOMS PARA PROMOCIONES COMO SER ENVIO A SPS
 # A CHOLOMA O OTRAS CIUDADES DESCUENTOS ALEATORIOS USANDO DICCIONARIOS
-#  
+#
 
 import time
 import os
@@ -14,8 +14,8 @@ import random
 
 historial_compras = []
 carrito_compras = []
-JAJJ
-pexi
+
+
 def valoracion_reseña():
   print("\ntomate un minuto de tu tiempo para evaluar el sistema de esta tienda")
   estrellas = [
